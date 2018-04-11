af788fca98f1e182adb8e94ea38bd5e511f11acb
Marcin Bogdanski <marcin@bogdanski.info>
2018-04-11 13:55:26 +0200

    some issue

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # zadanie_15_7
 Web Developer Bootcamp Kodilla ES6 timer on React;-)
 
+sprawdź jak to działa na stronie
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let timeTable = [];
+let timeTable = []
 
 class Stopwatch extends React.Component {
-  constructor(display) {
-   super(display); 
+  constructor (display) {
+    super(display) 
     this.state = {
       running: false,
       display: display,
@@ -10,52 +10,52 @@ class Stopwatch extends React.Component {
         minutes: 0,
         seconds: 0,
         miliseconds: 0
-      },
-    };
+      }
+    }
   }
-  reset() {
-    this.setState ({
+  reset () {
+    this.setState({
       running: false,
       times: {
         minutes: 0,
         seconds: 0,
         miliseconds: 0
-      },
-    });
+      }
+    })
   }
-  
-  format(times) {
-    return `${this.pad0(this.state.times.minutes)}:${this.pad0(this.state.times.seconds)}:${this.pad0(Math.floor(this.state.times.miliseconds))}`;
+
+  format (times) {
+    return `${this.pad0(this.state.times.minutes)}:${this.pad0(this.state.times.seconds)}:${this.pad0(Math.floor(this.state.times.miliseconds))}`
   }
-	
-  start() {
-	console.log(this.state.running);  
+
+  start () {
+    console.log(this.state.running)  
     if (!this.state.running) {
-      this.state.running = true;
-      this.watch = setInterval(() => this.step(), 10);
+      this.state.running = true
+      this.watch = setInterval(() => this.step(), 10)
     }
   }
-	
-  step() {
-    if (!this.state.running) return;
-    this.calculate();
-  }
-	
-  calculate() {
-    if (!this.state.running) return;
-    let miliseconds = this.state.times.miliseconds;
-    let seconds = this.state.times.seconds;
-    let minutes = this.state.times.minutes;
 
-    miliseconds++;
+  step () {
+    if (!this.state.running) return
+    this.calculate()
+  }
+
+  calculate () {
+    if (!this.state.running) return
+    let miliseconds = this.state.times.miliseconds
+    let seconds = this.state.times.seconds
+    let minutes = this.state.times.minutes
+
+    miliseconds++
 
     if (miliseconds >= 100) {
-      seconds += 1;
-      miliseconds = 0;
+      seconds += 1
+      miliseconds = 0
     }
     if (seconds >= 60) {
-      minutes += 1;
-      seconds = 0;
+      minutes += 1
+      seconds = 0
     }
 
     this.setState({
@@ -66,55 +66,55 @@ class Stopwatch extends React.Component {
       }
     })
   }
-	
-  stop() {
-    this.state.running = false;
-    clearInterval(this.watch);
+
+  stop () {
+    this.state.running = false
+    clearInterval(this.watch)
   }
-  
-  get() {
-    console.log(this.format(this.times));
-    timeTable = [...timeTable, this.format(this.times)];
-    console.log(timeTable);
+
+  get () {
+    console.log(this.format(this.times))
+    timeTable = [...timeTable, this.format(this.times)]
+    console.log(timeTable)
   }
-	
-  clear() {
-    timeTable = [];
-    console.log(timeTable);
-    document.getElementById('list').innerHTML= "";
+
+  clear () {
+    timeTable = []
+    console.log(timeTable)
+    document.getElementById('list').innerHTML = '';
   }
-	
-  results() {
-    let pos = timeTable.length - 1;
-    this.val = timeTable;
+
+  results () {
+    let pos = timeTable.length - 1
+    this.val = timeTable
     let addTime = timeTable[pos]
-    const res = document.getElementById('list');
-    const list = document.createElement("li");
-    list.innerText = addTime;
-    res.appendChild(list);
+    const res = document.getElementById('list')
+    const list = document.createElement('li')
+    list.innerText = addTime
+    res.appendChild(list)
   }
-  
-  pad0(value) {
-  let result = value.toString();
+
+  pad0 (value) {
+    let result = value.toString()
   if (result.length < 2) {
-    result = '0' + result;
+      result = '0' + result
   }
-  return result;
+    return result
  }
 
-  render() {
+  render () {
     return React.createElement('div', {},
       React.createElement('div', {className: 'controls'},
-      React.createElement('button', {onClick: () => this.start()}, 'start'),
-      React.createElement('button', {onClick: () => this.stop()}, 'stop'),
-      React.createElement('button', {onClick: () => this.reset()}, 'reset'),
-      React.createElement('button', {onClick: () => {this.get(), this.results()}}, 'save'),
-      React.createElement('button', {onClick: () => this.clear()}, 'clear list')
-    ),
-    React.createElement('div', {id: 'stopwatch'}, this.format())
+        React.createElement('button', {onClick: () => this.start()}, 'start'),
+        React.createElement('button', {onClick: () => this.stop()}, 'stop'),
+        React.createElement('button', {onClick: () => this.reset()}, 'reset'),
+        React.createElement('button', {onClick: () => { this.get(), this.results() }}, 'save'),
+        React.createElement('button', {onClick: () => this.clear()}, 'clear list')
+      ),
+      React.createElement('div', {id: 'stopwatch'}, this.format())
     )
   }
 }
 
-var element = React.createElement(Stopwatch);
-ReactDOM.render(element, document.getElementById('app'));
+var element = React.createElement(Stopwatch)
+ReactDOM.render(element, document.getElementById('app'))
